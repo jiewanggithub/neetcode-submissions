c2d7c67d89649cd26e9e0fd0ca2b4e7ba4a5b1e9
Jie Wang <123528287+jiewanggithub@users.noreply.github.com>
2026-07-08 21:37:48 -0400

    Add: n-queens-ii - submission-0

---
+++ Data Structures & Algorithms/n-queens-ii/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res = []
+        cols = []
+
+        def valid(r, c):
+            for R in range(r):
+                C = cols[R]
+                if C + R == c + r or C - R == c - r :
+                    return False
+            return True
+
+        def dfs(r, s):
+            if r == n:
+                res.append(["."*c + "Q" + "."*(n-1-c) for c in cols])
+                return 
+            
+            for c in s:
+                if valid(r, c):
+                    cols.append(c)
+                    dfs(r + 1, s - {c})
+                    cols.pop()
+        dfs(0, set(range(n)))
+        return len(res)
