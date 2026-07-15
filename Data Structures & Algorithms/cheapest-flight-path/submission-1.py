ed7776d0c8d97fa16f1b3c59075d5205b6ef9b50
Jie Wang <123528287+jiewanggithub@users.noreply.github.com>
2026-07-14 23:56:38 -0400

    Add: cheapest-flight-path - submission-1

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj = defaultdict(list)
+        for from_i, to_i, price_i in flights:
+            adj[from_i].append((price_i, to_i))
+        
+        min_heap = [(0, src, 0)]
+        max_flight = k + 1
+        while min_heap:
+            total_price, city, flight_used = heapq.heappop(min_heap)
+            
+            if city == dst:
+                return total_price
+            
+            if flight_used == max_flight:
+                continue
+
+            for price, nei in adj[city]:
+                heapq.heappush(min_heap, (total_price + price, nei, flight_used + 1))
+        return -1
