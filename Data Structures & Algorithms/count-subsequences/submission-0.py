e407b3b851d3e5756f995f06fc4874e71cd47b53
Jie Wang <123528287+jiewanggithub@users.noreply.github.com>
2026-08-16 17:14:07 +0800

    Add: count-subsequences - submission-0

---
+++ Data Structures & Algorithms/count-subsequences/submission-0.py
@@ -0,0 +1,18 @@
+from functools import cache 
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        
+        path = []
+        @cache 
+        def dfs(i, j):
+            if j == len(t):
+                return 1
+            
+            if i == len(s):
+                return 0
+            res = dfs(i + 1, j)
+
+            if s[i] == t[j]:
+                res += dfs(i + 1, j + 1)
+            return res
+        return dfs(0, 0)
